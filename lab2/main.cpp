3730a328afa044019bc9fd4c3ad421a3982ad719
Kovalevskaya Maria <162086232+kovalevskaya18@users.noreply.github.com>
2024-11-22 23:56:54 +0300

    Add files via upload

---
+++ lab2/main.cpp
@@ -0,0 +1,34 @@
+int main() {
+    {
+        int expectedDeterminant = 120;
+        Matrix<int> m = Matrix<int>::getSpecificDeterminant(5, expectedDeterminant);
+        if (determinant(m) != expectedDeterminant) {
+            std::cout << "Test failed for 5x5 matrix with determinant " << expectedDeterminant
+                << std::endl;
+        }
+    }
+
+    {
+        int expectedDeterminant = 12'345'678;
+        Matrix<int> m = Matrix<int>::getSpecificDeterminant(50, expectedDeterminant);
+        if (determinant(m) != expectedDeterminant) {
+            std::cerr << "Test failed for 50x50 matrix with determinant " << expectedDeterminant
+                << std::endl;
+        }
+    }
+
+    {
+        int expectedDeterminant = 12'345'678;
+        Matrix<int> m = Matrix<int>::getSpecificDeterminant(200, expectedDeterminant);
+        if (determinant(m) != expectedDeterminant) {
+            std::cerr << "Test failed for 200x200 matrix with determinant " << expectedDeterminant
+                << std::endl;
+        }
+    }
+
+    return 0;
+}
+// Для запуска всех тестов в командной строке надо написать
+// cmake -S . -B build
+// cmake --build build
+// cd build && ctest
